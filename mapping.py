d2935bac150be8b16e922f1f7515f5a06a9db9df
Joao Paulo <joaopege@gmail.com>
2026-02-27 00:56:33 -0300

    Started mapping, the program works and i can check the outputs of each key from the controller in real time.

--- mapping.py
+++ mapping.py
@@ -1,50 +1,3 @@
-import hid
-import time
-
-# Estes são os IDs exatos tirados da UBS Device Tree em System Information. (USB GAMEPAD)
-VENDOR_ID = 0x0810
-PRODUCT_ID = 0x0001
-
-print(f"Searching for the commands (VID: {hex(VENDOR_ID)}, PID: {hex(PRODUCT_ID)})...")
-
-try:
-    # Inicializa o dispositivo
-    gamepad = hid.device()
-    gamepad.open(VENDOR_ID, PRODUCT_ID)
-    
-    # Ativa o modo não-bloqueante para o loop correr suavemente
-    gamepad.set_nonblocking(True)
-    
-    print("Connected!")
-
-    # Guarda o último estado do comando (começa vazio)
-    last_report = None
-
-    while True:
-        # Lê o pacote de dados do comando
-        report = gamepad.read(64)
-        
-        # Se recebeu dados E esses dados forem diferentes dos últimos que vimos:
-        if report and report != last_report:
-            print(f"New Report: {report}")
-            
-            # Atualiza a nossa "memória" com o novo estado
-            last_report = report
-        
-        # Pausa para não sobrecarregar o processador
-        time.sleep(0.01)
-
-except IOError as ex:
-    print(f"Erro ao ligar ao comando: {ex}")
-    print("Verifique se o comando está ligado à porta USB.")
-except KeyboardInterrupt:
-    print("\nA encerrar o programa...")
-finally:
-    # Fecha a ligação de forma segura
-    if 'gamepad' in locals():
-        gamepad.close()
-
-
 # MAPPING BUTTONS:
 #-----------------------------------------------------------------------------------------------------------------------------#
 #   INPUT       |          MAPPING BUTTONS:                                                                                   #
@@ -77,7 +30,3 @@ finally:
 #   Y + X       |         [1, 128, 128, 127, 127, 159, 0, 0]                                                                  #
 #                                                                                                                             #
 #-----------------------------------------------------------------------------------------------------------------------------#
-
-
-
-
